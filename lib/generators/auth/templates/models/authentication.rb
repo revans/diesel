d79b091443b0da64596e9b2ebabbfa6443047ba6
rrevans <robert@codewranglers.org>
2015-04-07 16:52:13 -0700

    added with_token class method to look up a user by their token

--- lib/generators/auth/templates/models/authentication.rb
+++ lib/generators/auth/templates/models/authentication.rb
@@ -4,19 +4,31 @@ module Authentication
   extend ActiveSupport::Concern
 
   included do
+    # Rails' User based Authentcation base
     has_secure_password
-    validates_uniqueness_of :email
-    validates_presence_of :token
 
+    # Validations
+    validates_uniqueness_of :email
+    validates_presence_of   :token
+    validates_length_of     :password, minimum: 6
+
+    # Before Validation Filters
     before_validation :ensure_lowercase_email
     before_validation :set_token
   end
 
 
   module ClassMethods
+
+    # Class method to authenticate a user with an email & password
     def authenticate(email, password)
       User.find_by(email: email.downcase).
         try(:authenticate, password)
+    end
+
+
+    def with_token?(token)
+      User.where(token: token).first
     end
   end
 
